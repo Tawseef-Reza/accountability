66c121e534132ee5b3e6846643fe985e093ddda4
Tawseef_Reza <tawseefrezahome@gmail.com>
2026-06-01 23:38:30 -0400

    sample text

--- src/data/careerPosts.js
+++ src/data/careerPosts.js
@@ -4,6 +4,35 @@ const careerPosts = [
     title: 'Sample Post',
     description: 'Sample Description',
     content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget lobortis nisl, a dictum dui. Curabitur pulvinar tristique risus, non sollicitudin justo malesuada in. Donec vulputate erat eu metus fermentum, in congue nunc lobortis. Sed ac rutrum ipsum. Quisque accumsan, justo euismod eleifend sodales, mauris felis tempor nulla, a sodales justo magna et dui. Morbi porttitor est eget vestibulum interdum. Vestibulum nibh mi, suscipit at nunc eu, tristique luctus ipsum.',
+    date: 'June 1, 2026',
+  },
+  {
+    id: 2,
+    title: 'Sample Post',
+    description: 'Sample Description',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget lobortis nisl, a dictum dui. Curabitur pulvinar tristique risus, non sollicitudin justo malesuada in. Donec vulputate erat eu metus fermentum, in congue nunc lobortis. Sed ac rutrum ipsum. Quisque accumsan, justo euismod eleifend sodales, mauris felis tempor nulla, a sodales justo magna et dui. Morbi porttitor est eget vestibulum interdum. Vestibulum nibh mi, suscipit at nunc eu, tristique luctus ipsum.',
+    date: 'June 1, 2026',
+  },{
+    id: 3,
+    title: 'Sample Post',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget lobortis nisl, a dictum dui. Curabitur pulvinar tristique risus, non sollicitudin justo malesuada in. Donec vulputate erat eu metus fermentum, in congue nunc lobortis. Sed ac rutrum ipsum. Quisque accumsan, justo euismod eleifend sodales, mauris felis tempor nulla, a sodales justo magna et dui. Morbi porttitor est eget vestibulum interdum. Vestibulum nibh mi, suscipit at nunc eu, tristique luctus ipsum.',
+    content:
+      'Sample Content',
+    date: 'June 1, 2026',
+  },{
+    id: 4,
+    title: 'Sample Post',
+    description: 'Sample Description',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget lobortis nisl, a dictum dui. Curabitur pulvinar tristique risus, non sollicitudin justo malesuada in. Donec vulputate erat eu metus fermentum, in congue nunc lobortis. Sed ac rutrum ipsum. Quisque accumsan, justo euismod eleifend sodales, mauris felis tempor nulla, a sodales justo magna et dui. Morbi porttitor est eget vestibulum interdum. Vestibulum nibh mi, suscipit at nunc eu, tristique luctus ipsum.',
+    date: 'June 1, 2026',
+  },{
+    id: 5,
+    title: 'Sample Post',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget lobortis nisl, a dictum dui. Curabitur pulvinar tristique risus, non sollicitudin justo malesuada in. Donec vulputate erat eu metus fermentum, in congue nunc lobortis. Sed ac rutrum ipsum. Quisque accumsan, justo euismod eleifend sodales, mauris felis tempor nulla, a sodales justo magna et dui. Morbi porttitor est eget vestibulum interdum. Vestibulum nibh mi, suscipit at nunc eu, tristique luctus ipsum.',
+    content:
       'Sample Content',
     date: 'June 1, 2026',
   }
