c5c739b1ba5ff894ed6897def9871cb3c3f8b480
Tawseef_Reza <tawseefrezahome@gmail.com>
2026-06-01 20:01:47 -0400

    edited sample post

--- src/data/islamicPosts.js
+++ src/data/islamicPosts.js
@@ -1,11 +1,12 @@
 const islamicPosts = [
   {
     id: 1,
-    title: "Tawheed Basics",
-    description: "Understanding the oneness of Allah",
-    content: "Today I studied the basics of Tawheed...",
-    date: "June 1, 2026",
-  },
+    title: 'Sample Post',
+    description: 'Sample Description',
+    content:
+      'Sample Content',
+    date: 'June 1, 2026',
+  }
 ]
 
 export default islamicPosts
