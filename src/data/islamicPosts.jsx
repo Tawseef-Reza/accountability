5a88417666cbdf66587a9b2566400a7874f7690c
Tawseef_Reza <tawseefrezahome@gmail.com>
2026-06-19 19:15:04 -0400

    Changes

--- src/data/islamicPosts.jsx
+++ src/data/islamicPosts.jsx
@@ -18,7 +18,39 @@ const islamicPosts = [
 				</p>
 			</div>
 		,
-    date: 'June 19, 2026',
+    	date: 'June 19, 2026',
+	},
+	{
+		id: 2,
+		title: 'Lesson 2 Part 1 Ustadh Abdul-Aziz Al-Haqqan | Durus al-Muhimmah',
+		description: 'Learning about the first pillar for 30+ minutes',
+		content:
+			<div>
+				<h2>Lesson 2 Part 1</h2>
+				<p>
+					Today was around 44 minutes of studying the lesson 2 part 1 video from Ustadh Abdul-Aziz, which I got through 26 minutes of. 
+					The time was all pretty much regarding the first pillar of Islam. Aspects of that first pillar include affirmation and negation (both necessary to be a Muslim), 
+					the dangers of shirk, the 8 conditions of the shahaadah, and belief and disbelief being based upon actions of the heart, tongue, and limbs. 
+				</p>
+				<h3>Actions of the Limbs</h3>
+				<p> 
+					A big focus was on the importance of actions of the limbs. One hadith made tafsir of the shahaadatayn mentioned in the other hadith,
+					explaining the shahaadatayn as worshipping Allah, and not committing shirk. Worship of course is in the heart, by the tongue, 
+					and through actions.
+				</p>
+				<p>
+					Actions can nullify Islam as well, like actions of shirk (worshipping graves, making du'aa to other than Allah, prostrating for 
+					other than Allah).
+				</p>
+				<p>
+					Actions was also mentioned again in the condition of truthfulness, which was the last thing I took. Actions of the limbs are a part of 
+					truthfulness, since if you truly believed that no one is worthy of worship in truth other than Allah, you would take action to follow
+					the Quran and sunnah. You cannot say you have full iman and you don't do the actions commanded on you, just believing in your heart is 
+					not enough to be truthful. 
+				</p>
+			</div>
+		,
+    	date: 'June 19, 2026',
 	}
 ]
 
